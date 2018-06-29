18d7d109f44ef05004bd4c7d3f9c8b5ad8055d1c
Unknown <jlnagelk@student.avans.nl>
2018-06-29 15:56:22 +0200

    Setup for category is done
    * made all endpoints
    * made the skeleton for the methods of category
    * needs a database to actually function

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const bodyParser = require('body-parser')
-const ApiError = require('./model/ApiError')
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+const ApiError = require('./model/ApiError');
+const category_routes = require('./routes/category.routes')
 
 const port = 3000;
 
-app.use('/api', express.static('public')) 
+app.use('/api', express.static('public')); 
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.use('*', function(req, res, next){
     next()
 })
+
 //in comments because it has not yet been made or in use
 //app.use('/api', auth_routes)
 //app.all('*', AuthController.validateToken);
 
 //Here follow some regular endpoints
+app.use('api/category', category_routes);
 
 app.use((err, req, res, next) => {
-	console.dir(err)
-	res.status((err.code || 404)).json(err).end()	
+	console.dir(err);
+    const error = new ApiError("This endpoint doesn't exist", 404);
+    next(error);
 })
 
 app.listen(port, () => {
-	console.log('Server running on port ' + port)
+	console.log('Server running on port ' + port);
 })
 
 module.exports = app;
